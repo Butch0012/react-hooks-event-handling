172ac61214401713f4e155af30c4b909b4475330
butch0012 <elnathanmburu@gmail.com>
2023-11-27 21:05:05 +0300

    done

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -4,12 +4,12 @@ function MultiButton() {
   function handleClick(number) {
     console.log(`Button ${number} was clicked`);
   }
-
+/*  the console messages will appear as soon as our component is rendered, not when the button is clicked. This is why we always need to provide a function definition, not a function invocation to our event handlers.*/
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
     </div>
   );
 }
